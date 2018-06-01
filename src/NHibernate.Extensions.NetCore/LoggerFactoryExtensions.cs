649dfb45d66656041481d6940e1879955078e100
beginor <beginor@qq.com>
2018-06-01 11:12:55 +0800

    add extensions to .net core

---
+++ src/NHibernate.Extensions.NetCore/LoggerFactoryExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace NHibernate.Extensions.NetCore {
+
+    public static class LoggerFactoryExtensions {
+
+        public static void UseAsNHibernateLoggerFactory(
+            this Microsoft.Extensions.Logging.ILoggerFactory loggerFactory
+        ) {
+            NHibernateLogger.SetLoggersFactory(
+                new NetCoreLoggerFactory(loggerFactory)
+            );
+        }
+
+    }
+
+}
