a050606852a896cf3105bb997a3fbb0d1929362d
beginor <beginor@qq.com>
2018-06-14 14:18:37 +0800

    add a test web app

---
+++ test/NHibernate.Extensions.WebTest/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace NHibernate.Extensions.WebTest.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
