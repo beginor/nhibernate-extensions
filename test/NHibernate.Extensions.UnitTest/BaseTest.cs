304be632d07585959e3e0d0136cef95f59d3b7ff
beginor <beginor@qq.com>
2018-12-28 16:28:47 +0800

    update test.

--- test/NHibernate.Extensions.UnitTest/BaseTest.cs
+++ test/NHibernate.Extensions.UnitTest/BaseTest.cs
@@ -1,7 +1,12 @@
 using System;
 using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.Extensions.DependencyInjection;
 using NHibernate.Cfg;
+using NHibernate.Extensions.UnitTest.DvdRental;
+using NHibernate.Extensions.UnitTest.TestDb;
+using NHibernate.Linq;
 using NHibernate.NetCore;
 using NHibernate.Mapping.Attributes;
 using Npgsql;
@@ -9,7 +14,7 @@ using NUnit.Framework;
 
 namespace NHibernate.Extensions.UnitTest {
 
-    public abstract class BaseTest {
+    public class BaseTest {
 
         protected IServiceProvider ServiceProvider { get; private set; }
 
@@ -36,6 +41,29 @@ namespace NHibernate.Extensions.UnitTest {
             services.AddHibernate("dvd_rental", dvdRental);
             // build service provider
             ServiceProvider = services.BuildServiceProvider();
+        }
+
+        [Test]
+        public void _01_CanResolveSessionFactories() {
+            Assert.NotNull(ServiceProvider);
+            Assert.NotNull(TestDbSessionFactory);
+            Assert.NotNull(DvdRentalSessionFactory);
+        }
+
+        [Test]
+        public async Task _02_CanQueryTestDb() {
+            using (var session = TestDbSessionFactory.OpenSession()) {
+                var author = await session.Query<Author>().FirstOrDefaultAsync();
+                Assert.NotNull(author);
+            }
+        }
+
+        [Test]
+        public async Task _03_CanQueryDvdRental() {
+            using (var session = DvdRentalSessionFactory.OpenSession()) {
+                var actor = await session.Query<Actor>().FirstOrDefaultAsync();
+                Assert.NotNull(actor);
+            }
         }
 
     }
