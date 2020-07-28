52ed02a2e521d4986f656066922cbbd2df47025d
beginor <beginor@qq.com>
2020-07-28 16:14:43 +0800

    update package version

--- push-package.sh
+++ push-package.sh
@@ -1,12 +1,12 @@
 #!/bin/bash -e
 dotnet pack src/NHibernate.NetCore/NHibernate.NetCore.csproj -c Release
-dotnet nuget push src/NHibernate.NetCore/bin/Release/NHibernate.NetCore.3.1.4.nupkg -s nuget.org -k $(cat ~/.nuget/key.txt)
+dotnet nuget push src/NHibernate.NetCore/bin/Release/NHibernate.NetCore.3.1.5.nupkg -s nuget.org -k $(cat ~/.nuget/key.txt)
 rm -rf src/NHibernate.NetCore/bin/Release
 
 dotnet pack src/NHibernate.Extensions.NpgSql/NHibernate.Extensions.NpgSql.csproj -c Release
-dotnet nuget push src/NHibernate.Extensions.NpgSql/bin/Release/NHibernate.Extensions.NpgSql.3.1.6.nupkg -s nuget.org -k $(cat ~/.nuget/key.txt)
+dotnet nuget push src/NHibernate.Extensions.NpgSql/bin/Release/NHibernate.Extensions.NpgSql.3.1.7.nupkg -s nuget.org -k $(cat ~/.nuget/key.txt)
 rm -rf src/NHibernate.Extensions.NpgSql/bin/Release
 
 dotnet pack src/NHibernate.Extensions.Sqlite/NHibernate.Extensions.Sqlite.csproj -c Release
-dotnet nuget push src/NHibernate.Extensions.Sqlite/bin/Release/NHibernate.Extensions.Sqlite.2.0.3.nupkg -s nuget.org -k $(cat ~/.nuget/key.txt)
+dotnet nuget push src/NHibernate.Extensions.Sqlite/bin/Release/NHibernate.Extensions.Sqlite.2.0.4.nupkg -s nuget.org -k $(cat ~/.nuget/key.txt)
 rm -rf src/NHibernate.Extensions.Sqlite/bin/Release
