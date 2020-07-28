7cba6578d61e4b55135a412439a28a01e6f13ead
beginor <beginor@qq.com>
2020-07-28 16:09:28 +0800

    refact NpgSqlDialet

---
+++ src/NHibernate.Extensions.NpgSql/NpgSqlDialet.cs
@@ -0,0 +1,50 @@
+using NHibernate.SqlTypes;
+using NpgsqlTypes;
+
+namespace NHibernate.Extensions.NpgSql {
+
+    public class NpgSqlDialet : NHibernate.Dialect.PostgreSQL83Dialect {
+
+        public override string GetTypeName(
+            SqlType sqlType
+        ) {
+            if (sqlType is NpgSqlType npgSqlType) {
+                var npgDbType = npgSqlType.NpgDbType;
+                if (npgDbType == NpgsqlDbType.Json) {
+                    return "json";
+                }
+                if (npgDbType == NpgsqlDbType.Jsonb) {
+                    return "jsonb";
+                }
+                // array type
+                if (npgDbType < 0) {
+                    var arrayDbType = (NpgsqlDbType) (npgDbType - NpgsqlDbType.Array);
+                    if (arrayDbType == NpgsqlDbType.Boolean) {
+                        return "boolean[]";
+                    }
+                    if (arrayDbType == NpgsqlDbType.Double) {
+                        return "double precision[]";
+                    }
+                    if (arrayDbType == NpgsqlDbType.Real) {
+                        return "real[]";
+                    }
+                    if (arrayDbType == NpgsqlDbType.Smallint) {
+                        return "smallint[]";
+                    }
+                    if (arrayDbType == NpgsqlDbType.Integer) {
+                        return "integer[]";
+                    }
+                    if (arrayDbType == NpgsqlDbType.Bigint) {
+                        return "bigint[]";
+                    }
+                    if (arrayDbType == NpgsqlDbType.Varchar) {
+                        return "character varying[]";
+                    }
+                }
+            }
+            return base.GetTypeName(sqlType);
+        }
+
+    }
+
+}
--- src/NHibernate.Extensions.NpgSql/NpgSqlDriver.cs
+++ src/NHibernate.Extensions.NpgSql/NpgSqlDriver.cs
@@ -3,7 +3,6 @@ using System.Data.Common;
 using NHibernate.Driver;
 using NHibernate.SqlTypes;
 using Npgsql;
-using NpgsqlTypes;
 
 namespace NHibernate.Extensions.NpgSql {
 
@@ -42,47 +41,4 @@ namespace NHibernate.Extensions.NpgSql {
         }
     }
 
-    public class NpgSqlDialet : NHibernate.Dialect.PostgreSQL83Dialect {
-
-        public override string GetTypeName(
-            SqlType sqlType
-        ) {
-            if (sqlType is NpgSqlType npgSqlType) {
-                var npgDbType = npgSqlType.NpgDbType;
-                if (npgDbType == NpgsqlDbType.Json) {
-                    return "json";
-                }
-                if (npgDbType == NpgsqlDbType.Jsonb) {
-                    return "jsonb";
-                }
-                // array type
-                if (npgDbType < 0) {
-                    var arrayDbType = (NpgsqlDbType) (npgDbType - NpgsqlDbType.Array);
-                    if (arrayDbType == NpgsqlDbType.Boolean) {
-                        return "boolean[]";
-                    }
-                    if (arrayDbType == NpgsqlDbType.Double) {
-                        return "double precision[]";
-                    }
-                    if (arrayDbType == NpgsqlDbType.Real) {
-                        return "real[]";
-                    }
-                    if (arrayDbType == NpgsqlDbType.Smallint) {
-                        return "smallint[]";
-                    }
-                    if (arrayDbType == NpgsqlDbType.Integer) {
-                        return "integer[]";
-                    }
-                    if (arrayDbType == NpgsqlDbType.Bigint) {
-                        return "bigint[]";
-                    }
-                    if (arrayDbType == NpgsqlDbType.Varchar) {
-                        return "character varying[]";
-                    }
-                }
-            }
-            return base.GetTypeName(sqlType);
-        }
-
-    }
 }
