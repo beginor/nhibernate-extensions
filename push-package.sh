409dfab5137979cdc3c766dd7896802ab00203de
beginor <beginor@qq.com>
2022-10-03 16:31:52 +0800

    update version

--- push-package.sh
+++ push-package.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -e
-PACKAGE_VERSION="6.0.11"
+PACKAGE_VERSION="6.0.12"
 dotnet pack src/NHibernate.NetCore/NHibernate.NetCore.csproj -c Release
 dotnet nuget push src/NHibernate.NetCore/bin/Release/NHibernate.NetCore.$PACKAGE_VERSION.nupkg -s nuget.org -k $(cat ~/.nuget/key.txt)
 rm -rf src/NHibernate.NetCore/bin/Release
